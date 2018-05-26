39ec159051ad055a7918429583d2f23422209cdb
chandraat <chandraat@ncs.com.sg>
2018-05-26 13:21:22 +0800

    Initial Commit

---
+++ WeatherRecordsInHourly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherRecordsInHourly
+//
+//  Created by NCS_Piraba on 26/5/18.
+//  Copyright © 2018 Piraba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
